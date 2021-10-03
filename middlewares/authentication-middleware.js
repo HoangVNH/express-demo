a031f959fe6cdff8bf846e3d8d1203cb3e816f48
Henry Beck <terminator0123456@yahoo.com>
2021-10-03 23:40:58 +0700

    enhance code

--- middlewares/authentication-middleware.js
+++ middlewares/authentication-middleware.js
@@ -9,7 +9,7 @@ const authenticationMiddleware = async (req, res, next) => {
 
     var decoded = null;
     try {
-        decoded = jwt.verify(accessToken.split(' ')[1], process.env.PRIVATE_KEY);
+        decoded = await jwt.verify(accessToken.split(' ')[1], process.env.PRIVATE_KEY);
     } catch (error) {
         return res.status(401).json('Unauthorized access');
     }
